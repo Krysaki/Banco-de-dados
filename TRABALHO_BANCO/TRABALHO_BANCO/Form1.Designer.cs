1ff7188d61bf98ec115366985b2c9682d64ed01a
Nicholas Zortea Graczik <78584142+NicholasZortea@users.noreply.github.com>
2022-10-20 21:57:46 -0300

    botao Deletar funcionando

--- TRABALHO_BANCO/TRABALHO_BANCO/Form1.Designer.cs
+++ TRABALHO_BANCO/TRABALHO_BANCO/Form1.Designer.cs
@@ -42,42 +42,45 @@
             this.btDeletar = new System.Windows.Forms.Button();
             this.btConsultar = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.tRABALHO_BANCODataSet = new TRABALHO_BANCO.TRABALHO_BANCODataSet();
-            this.cLIENTEBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.cLIENTETableAdapter = new TRABALHO_BANCO.TRABALHO_BANCODataSetTableAdapters.CLIENTETableAdapter();
             this.cLIIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cLINOMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cLICPFDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cLITELEFONEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cLIEMAILDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cLIENTEBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tRABALHO_BANCODataSet = new TRABALHO_BANCO.TRABALHO_BANCODataSet();
+            this.cLIENTETableAdapter = new TRABALHO_BANCO.TRABALHO_BANCODataSetTableAdapters.CLIENTETableAdapter();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tRABALHO_BANCODataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.cLIENTEBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tRABALHO_BANCODataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 28);
+            this.label1.Location = new System.Drawing.Point(32, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(47, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txName
             // 
-            this.txName.Location = new System.Drawing.Point(68, 21);
+            this.txName.Location = new System.Drawing.Point(91, 26);
+            this.txName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txName.Name = "txName";
-            this.txName.Size = new System.Drawing.Size(111, 20);
+            this.txName.Size = new System.Drawing.Size(147, 22);
             this.txName.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 107);
+            this.label2.Location = new System.Drawing.Point(32, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.Size = new System.Drawing.Size(36, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "CPF:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -85,49 +88,56 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 67);
+            this.label3.Location = new System.Drawing.Point(32, 82);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.Size = new System.Drawing.Size(64, 16);
             this.label3.TabIndex = 3;
             this.label3.Text = "Telefone:";
             // 
             // mtxTelefone
             // 
-            this.mtxTelefone.Location = new System.Drawing.Point(79, 60);
+            this.mtxTelefone.Location = new System.Drawing.Point(105, 74);
+            this.mtxTelefone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.mtxTelefone.Mask = "(00) 00000-0000";
             this.mtxTelefone.Name = "mtxTelefone";
-            this.mtxTelefone.Size = new System.Drawing.Size(100, 20);
+            this.mtxTelefone.Size = new System.Drawing.Size(132, 22);
             this.mtxTelefone.TabIndex = 4;
             // 
             // mtxCPF
             // 
-            this.mtxCPF.Location = new System.Drawing.Point(68, 100);
+            this.mtxCPF.Location = new System.Drawing.Point(91, 123);
+            this.mtxCPF.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.mtxCPF.Mask = "000,000,000-00";
             this.mtxCPF.Name = "mtxCPF";
-            this.mtxCPF.Size = new System.Drawing.Size(111, 20);
+            this.mtxCPF.Size = new System.Drawing.Size(147, 22);
             this.mtxCPF.TabIndex = 5;
+            this.mtxCPF.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mtxCPF_MaskInputRejected);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(27, 143);
+            this.label4.Location = new System.Drawing.Point(36, 176);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(44, 16);
             this.label4.TabIndex = 6;
             this.label4.Text = "Email:";
             // 
             // txEmail
             // 
-            this.txEmail.Location = new System.Drawing.Point(79, 136);
+            this.txEmail.Location = new System.Drawing.Point(105, 167);
+            this.txEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txEmail.Name = "txEmail";
-            this.txEmail.Size = new System.Drawing.Size(100, 20);
+            this.txEmail.Size = new System.Drawing.Size(132, 22);
             this.txEmail.TabIndex = 7;
             // 
             // btInserir
             // 
-            this.btInserir.Location = new System.Drawing.Point(248, 19);
+            this.btInserir.Location = new System.Drawing.Point(331, 23);
+            this.btInserir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btInserir.Name = "btInserir";
-            this.btInserir.Size = new System.Drawing.Size(75, 23);
+            this.btInserir.Size = new System.Drawing.Size(100, 28);
             this.btInserir.TabIndex = 8;
             this.btInserir.Text = "Inserir";
             this.btInserir.UseVisualStyleBackColor = true;
@@ -135,27 +145,31 @@
             // 
             // btAlterar
             // 
-            this.btAlterar.Location = new System.Drawing.Point(248, 57);
+            this.btAlterar.Location = new System.Drawing.Point(331, 70);
+            this.btAlterar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btAlterar.Name = "btAlterar";
-            this.btAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btAlterar.Size = new System.Drawing.Size(100, 28);
             this.btAlterar.TabIndex = 9;
             this.btAlterar.Text = "Alterar";
             this.btAlterar.UseVisualStyleBackColor = true;
             // 
             // btDeletar
             // 
-            this.btDeletar.Location = new System.Drawing.Point(248, 133);
+            this.btDeletar.Location = new System.Drawing.Point(331, 164);
+            this.btDeletar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btDeletar.Name = "btDeletar";
-            this.btDeletar.Size = new System.Drawing.Size(75, 23);
+            this.btDeletar.Size = new System.Drawing.Size(100, 28);
             this.btDeletar.TabIndex = 10;
             this.btDeletar.Text = "Deletar";
             this.btDeletar.UseVisualStyleBackColor = true;
+            this.btDeletar.Click += new System.EventHandler(this.btDeletar_Click);
             // 
             // btConsultar
             // 
-            this.btConsultar.Location = new System.Drawing.Point(248, 97);
+            this.btConsultar.Location = new System.Drawing.Point(331, 119);
+            this.btConsultar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btConsultar.Name = "btConsultar";
-            this.btConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btConsultar.Size = new System.Drawing.Size(100, 28);
             this.btConsultar.TabIndex = 11;
             this.btConsultar.Text = "Consultar";
             this.btConsultar.UseVisualStyleBackColor = true;
@@ -171,29 +185,18 @@
             this.cLITELEFONEDataGridViewTextBoxColumn,
             this.cLIEMAILDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.cLIENTEBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(79, 224);
+            this.dataGridView1.Location = new System.Drawing.Point(105, 276);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(659, 187);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(879, 230);
             this.dataGridView1.TabIndex = 12;
-            // 
-            // tRABALHO_BANCODataSet
-            // 
-            this.tRABALHO_BANCODataSet.DataSetName = "TRABALHO_BANCODataSet";
-            this.tRABALHO_BANCODataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // cLIENTEBindingSource
-            // 
-            this.cLIENTEBindingSource.DataMember = "CLIENTE";
-            this.cLIENTEBindingSource.DataSource = this.tRABALHO_BANCODataSet;
-            // 
-            // cLIENTETableAdapter
-            // 
-            this.cLIENTETableAdapter.ClearBeforeFill = true;
             // 
             // cLIIDDataGridViewTextBoxColumn
             // 
             this.cLIIDDataGridViewTextBoxColumn.DataPropertyName = "CLI_ID";
             this.cLIIDDataGridViewTextBoxColumn.HeaderText = "CLI_ID";
+            this.cLIIDDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.cLIIDDataGridViewTextBoxColumn.Name = "cLIIDDataGridViewTextBoxColumn";
             this.cLIIDDataGridViewTextBoxColumn.Width = 70;
             // 
@@ -201,6 +204,7 @@
             // 
             this.cLINOMEDataGridViewTextBoxColumn.DataPropertyName = "CLI_NOME";
             this.cLINOMEDataGridViewTextBoxColumn.HeaderText = "CLI_NOME";
+            this.cLINOMEDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.cLINOMEDataGridViewTextBoxColumn.Name = "cLINOMEDataGridViewTextBoxColumn";
             this.cLINOMEDataGridViewTextBoxColumn.Width = 150;
             // 
@@ -208,6 +212,7 @@
             // 
             this.cLICPFDataGridViewTextBoxColumn.DataPropertyName = "CLI_CPF";
             this.cLICPFDataGridViewTextBoxColumn.HeaderText = "CLI_CPF";
+            this.cLICPFDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.cLICPFDataGridViewTextBoxColumn.Name = "cLICPFDataGridViewTextBoxColumn";
             this.cLICPFDataGridViewTextBoxColumn.Width = 125;
             // 
@@ -215,6 +220,7 @@
             // 
             this.cLITELEFONEDataGridViewTextBoxColumn.DataPropertyName = "CLI_TELEFONE";
             this.cLITELEFONEDataGridViewTextBoxColumn.HeaderText = "CLI_TELEFONE";
+            this.cLITELEFONEDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.cLITELEFONEDataGridViewTextBoxColumn.Name = "cLITELEFONEDataGridViewTextBoxColumn";
             this.cLITELEFONEDataGridViewTextBoxColumn.Width = 120;
             // 
@@ -222,14 +228,29 @@
             // 
             this.cLIEMAILDataGridViewTextBoxColumn.DataPropertyName = "CLI_EMAIL";
             this.cLIEMAILDataGridViewTextBoxColumn.HeaderText = "CLI_EMAIL";
+            this.cLIEMAILDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.cLIEMAILDataGridViewTextBoxColumn.Name = "cLIEMAILDataGridViewTextBoxColumn";
             this.cLIEMAILDataGridViewTextBoxColumn.Width = 150;
             // 
+            // cLIENTEBindingSource
+            // 
+            this.cLIENTEBindingSource.DataMember = "CLIENTE";
+            this.cLIENTEBindingSource.DataSource = this.tRABALHO_BANCODataSet;
+            // 
+            // tRABALHO_BANCODataSet
+            // 
+            this.tRABALHO_BANCODataSet.DataSetName = "TRABALHO_BANCODataSet";
+            this.tRABALHO_BANCODataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cLIENTETableAdapter
+            // 
+            this.cLIENTETableAdapter.ClearBeforeFill = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btConsultar);
             this.Controls.Add(this.btDeletar);
@@ -243,12 +264,13 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txName);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tRABALHO_BANCODataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.cLIENTEBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tRABALHO_BANCODataSet)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- TRABALHO_BANCO/TRABALHO_BANCO/Form1.cs
+++ TRABALHO_BANCO/TRABALHO_BANCO/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -35,6 +36,86 @@ namespace TRABALHO_BANCO
         }
 
         private void btInserir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btDeletar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn;
+            SqlCommand comm;
+            bool isOK = true;
+
+            // Recupera a string de conexão com os dados do servidor e da base de dados
+            string connectionString = Properties.Settings.Default.TRABALHO_BANCOConnectionString;
+
+            // Iniciar a conexão com o Banco de Dados
+            conn = new SqlConnection(connectionString);
+
+            // Cria o comando SQL
+            comm = new SqlCommand(
+                "DELETE FROM CLIENTE " +
+                "WHERE CLI_CPF = @CLI_CPF", conn);
+
+            // Declara e atribui os valores para as variáveis
+            comm.Parameters.Add("@CLI_CPF", System.Data.SqlDbType.NVarChar);
+            comm.Parameters["@CLI_CPF"].Value = mtxCPF.Text;
+
+
+            try
+            {
+                // Tentar abrir uma conexão com o Servidor/Base de dados
+                try
+                {
+                    conn.Open();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message,
+                        "Erro ao abrir conexão com o Banco de Dados",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+
+                    isOK = false;
+                }
+
+                // Tentar executar o comando SQL
+                try
+                {
+                    comm.ExecuteNonQuery();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message,
+                        "Erro ao tentar executar o comando SQL",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+
+                    isOK = false;
+                }
+            }
+            catch { }
+            finally
+            {
+                conn.Close(); // Fecha a conexão com o Banco de Dados
+
+                if (isOK == true)
+                {
+                    MessageBox.Show("Cliente deletado com sucesso!",
+                            "DELETE",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+
+                    // Atualizar a lista de cidades 
+                    this.cLIENTETableAdapter.Fill(this.tRABALHO_BANCODataSet.CLIENTE);
+                }
+            }
+
+            //LimparForm();
+
+        } // FIM DO EVENTO btExcluir_Click
+
+        private void mtxCPF_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
         {
 
         }
