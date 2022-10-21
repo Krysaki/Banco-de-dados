6223f4a501c23634c63413abf58a4b52450a3b1e
Nicholas Zortea Graczik <78584142+NicholasZortea@users.noreply.github.com>
2022-10-20 21:21:21 -0300

    Remocao .zip

---
+++ TRABALHO_BANCO/TRABALHO_BANCO/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TRABALHO_BANCO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'tRABALHO_BANCODataSet.CLIENTE' table. You can move, or remove it, as needed.
+            this.cLIENTETableAdapter.Fill(this.tRABALHO_BANCODataSet.CLIENTE);
+
+        }
+
+        private void btInserir_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
